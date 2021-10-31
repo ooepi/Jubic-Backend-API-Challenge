f0499cd6596b64a7f8043a60c71d04c1c3e61e65
ooepi <robert.harmaakivi@gmail.com>
2021-10-31 11:43:28 +0200

    Initial commit

---
+++ src/main/java/com/roha/jubicbackendAPI/JubicbackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.roha.jubicbackendAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JubicbackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JubicbackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roha/jubicbackendAPI/JubicbackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roha.jubicbackendAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JubicbackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
